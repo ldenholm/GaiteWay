a6aebb01da903688123c9fe1ffab71b78d865e06
loch <disrute@gmail.com>
2021-03-04 16:25:20 +1100

    Add project files.

---
+++ Playstation.Domain/Models/Playstation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Playstation.Domain.Models
+{
+    public class Playstation
+    {
+        public string ModelName { get; set; }
+        public string YearReleased { get; set; }
+        public decimal Price { get; set; }
+        public bool IsSpecialEdition { get; set; }
+    }
+}
---
+++ Xbox.Domain/Models/Xbox.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xbox.Domain.Enums;
+
+namespace Xbox.Domain.Models
+{
+    public class Xbox
+    {
+        public string ModelName { get; set; }
+        public string YearReleased { get; set; }
+        public ControllerConfig Config { get; set; }
+        public decimal Price { get; set; }
+    }
+}
